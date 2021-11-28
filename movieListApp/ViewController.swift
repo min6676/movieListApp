9cbdedeaea09757d778484c663f9cf7a0b2e4891
butga@naver.com <butga@naver.com>
2021-11-28 13:36:33 +0900

    Initial Commit

---
+++ movieListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieListApp
+//
+//  Created by 김민순 on 2021/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
